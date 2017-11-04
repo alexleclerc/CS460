1a096048171cca651f00d8396904a257937a97ce
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-04 15:02:08 -0700

    Added XML Documentation.

--- homework3/calculator/calculator/Calculator.cs
+++ homework3/calculator/calculator/Calculator.cs
@@ -7,15 +7,28 @@ using System.Text.RegularExpressions;
 
 namespace CalculatorApp
 {
+    /// <summary>
+    /// This class holds the Main function as well as 
+    /// the functions nessecary to perform arithmetic
+    /// operations on user input. It has a stack that
+    /// is accessible to all inner functions.
+    /// </summary>
     class Calculator
     {
         private IStackADT Stack = new LinkedStack();
-
+        
+        /// <summary>
+        /// Creates an instance of the calculator app
+        /// and handles running the the app until the 
+        /// user quits.
+        /// </summary>
         static void Main(string[] args)
         {
             Calculator App = new Calculator();
             bool PlayAgain = true;
             Console.WriteLine("Postfix Calculator. Recognizes these operators: + - * /");
+
+            //keep the app open until the user input sets PlayAgain to false
             while (PlayAgain)
             {
                 PlayAgain = App.DoCalculation();
@@ -23,19 +36,33 @@ namespace CalculatorApp
             Console.WriteLine("Bye.");
         }
 
+        /// <summary>
+        /// Handles the I/O and directs the user input to the 
+        /// functions that do calculation.
+        /// </summary>
+        /// <returns>
+        /// Returns false when the user wishes 
+        /// to quit (by typing 'Q' or 'q') and true otherwise
+        /// </returns>
         private bool DoCalculation()
         {
-            Console.WriteLine("Enter 'q' to quit.");
+            //variables with default values just in case
             string Input = "2 2 +";
-            Console.Write("> ");
             string Output = "4";
+
+            //user prompt
+            Console.WriteLine("Enter 'q' to quit.");
+            Console.Write("> ");
+            
             Input = Console.ReadLine();
 
+            //if the user wants to exit we return false to quit the program
             if (Input.StartsWith("q") || Input.StartsWith("Q"))
             {
                 return false;
             }
 
+            //try to get the output, or retrieve any error messages.
             try
             {
                 Output = EvaluatePostFixInput(Input);
@@ -49,6 +76,21 @@ namespace CalculatorApp
             return true;
         }
 
+        /// <summary>
+        /// Evaluates user input, checking for errors,
+        /// and returns the proper arithmetic result
+        /// by calling DoOperation.
+        /// 
+        /// Uses the Stack to handle input and allow 
+        /// other functions access.
+        /// </summary>
+        /// <param name="input">
+        /// User input scanned from console
+        /// </param>
+        /// <returns>
+        /// Pops the result of the calculation off
+        /// the stack as a string.
+        /// </returns>
         public string EvaluatePostFixInput(string input)
         {
             
@@ -71,17 +113,29 @@ namespace CalculatorApp
                 Stack.Push(double.Parse(InputMatch.Groups[1].Value));
                 Stack.Push(double.Parse(InputMatch.Groups[2].Value));
                 Stack.Push(InputMatch.Groups[3].Value.ToString());
+                //use DoOperation to find the result
                 result = DoOperation();
                 
             }
+            //or throw an error if the input doesn't match our criteria
             else
             {
                 throw new ArgumentException("Improper input. (not in 'a b +' form)", input);
             }
+            //push the result onto the top of the stack so it is the next thing to be popped off.
             Stack.Push(result);
             return Stack.Pop().ToString();
         }
 
+        /// <summary>
+        /// Takes three items off the stack, checks the
+        /// operator type and does the appropriate calculation.
+        /// Division is a special case that checks for errors
+        /// that may come about from exceptional input.
+        /// </summary>
+        /// <returns>
+        /// The result of the operation if successful.
+        /// </returns>
         public double DoOperation()
         {
             string op; //operator
