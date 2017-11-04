44e3fa44843df2fae339e4fa19f8f371cd763043
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-03 19:12:32 -0700

    Testing regular expression.

--- homework3/calculator/calculator/Calculator.cs
+++ homework3/calculator/calculator/Calculator.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CalculatorApp
 {
     class Calculator
     {
-        //private IStackADT Stack = new LinkedStack();
+        private IStackADT Stack = new LinkedStack();
 
         static void Main(string[] args)
         {
@@ -37,10 +38,34 @@ namespace CalculatorApp
 
             string Output = "4";
 
+            EvaluatePostFixInput(Input);
+
             Console.WriteLine(Input);
             Console.WriteLine(Output);
 
             return true;
+        }
+
+        public string EvaluatePostFixInput(string input)
+        {
+            
+            if (input == null || input.Equals(""))
+            {
+                throw new ArgumentException("Null or empty strings are not valid.", input);
+            }
+            Stack.Clear();
+
+            string Pattern = @"(\d+)\s(\d+)\s([\+\-\*\/])";
+            Match InputMatch = Regex.Match(input, Pattern);
+            if (InputMatch.Success)
+            {
+                Console.WriteLine("'{0}' in '{1}' was successful.", InputMatch.Value, input);
+            }
+            else
+            {
+                Console.WriteLine("'{0}' in '{1}' was not successful.", InputMatch.Value, input);
+            }
+            return "nope";
         }
     }
 }
