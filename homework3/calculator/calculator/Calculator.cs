bb883e5a35ad5b90910063bf48eaf5f6d0fc0ca5
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-03 20:02:11 -0700

    Regex functions now parse input.

--- homework3/calculator/calculator/Calculator.cs
+++ homework3/calculator/calculator/Calculator.cs
@@ -55,11 +55,15 @@ namespace CalculatorApp
             }
             Stack.Clear();
 
-            string Pattern = @"(\d+)\s(\d+)\s([\+\-\*\/])";
+            //look for one or more digits (optionally with decimals)
+            //followed separted by a space, followed by an operator
+            string Pattern = @"(\d+(?:\.\d+)*)\s(\d+(?:\.\d+)*)\s*([\+\-\*\/])";
             Match InputMatch = Regex.Match(input, Pattern);
             if (InputMatch.Success)
             {
-                Console.WriteLine("'{0}' in '{1}' was successful.", InputMatch.Value, input);
+                //Console.WriteLine("'{0}' in '{1}' was successful.", InputMatch.Value, input);
+                Console.WriteLine("number one : {0}, number two : {1}, operator: {2}",
+                    InputMatch.Groups[1], InputMatch.Groups[2], InputMatch.Groups[3]);
             }
             else
             {
