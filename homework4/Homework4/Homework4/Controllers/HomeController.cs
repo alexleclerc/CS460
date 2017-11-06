2a4a39601267a1e7b1f9cd85ae8235845bacabc8
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-05 17:43:39 -0800

    Page 1 form handling.

--- homework4/Homework4/Homework4/Controllers/HomeController.cs
+++ homework4/Homework4/Homework4/Controllers/HomeController.cs
@@ -9,12 +9,39 @@ namespace Homework4.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        /// <summary>
+        /// The home page of the site.
+        /// </summary>
+        /// <returns>Website view</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Page 1 contains a bad password maker.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult Page1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Page1")]
+        public ActionResult Result1()
+        {
+            double measure = Convert.ToDouble(Request.Form["measure"]);
+            string unit = Request.Form["unit"];
+            //double result;
+
+            return Content($"The result is:{unit}");
+        }
+
+
+
+        public ActionResult Page2()
         {
             return View();
         }
