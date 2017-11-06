17e8b7940d63c2ce82740788ee0dfcd8759b9274
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-05 22:34:29 -0800

    Add Homework 5 View, Controller, and Model.

---
+++ homework5/Homework5/Controllers/FormController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework5.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Address()
+        {
+            return View();
+        }
+    }
+}
