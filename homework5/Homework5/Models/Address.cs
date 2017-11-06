47aeafb68bf7c04fce9cb1a180e9f8fd3f6bba12
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-06 09:50:33 -0800

    Finished the functionality.

--- homework5/Homework5/Models/Address.cs
+++ homework5/Homework5/Models/Address.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace Homework5.Models
 {
     public class Address
     {
+        public int ID { get; set; }
+        public DateTime dob { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Addr { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string County { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
     }
 }
