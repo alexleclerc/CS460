b24819ccb798ba1ad83a844be3e4551b09588f7c
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-11-07 17:29:51 -0800

    Added local database and configured it.

--- homework5/Homework5/Models/Address.cs
+++ homework5/Homework5/Models/Address.cs
@@ -1,24 +1,43 @@
 ﻿using System;
-using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Homework5.Models
 {
     public class Address
     {
+        [Display(Name = "ID Number:"), Required]
         public int ID { get; set; }
-        public DateTime dob { get; set; }
+
+        [Display(Name = "Date of Birth:"), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy"), Required]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Last Name:"), Required]
         public string LastName { get; set; }
+
+        [Display(Name = "First Name:"), Required]
         public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name:")]
         public string MiddleName { get; set; }
+
+        [Display(Name = "Address:"), Required]
         public string Addr { get; set; }
+
+        [Display(Name = "City:"), Required]
         public string City { get; set; }
+
+        [Display(Name = "State:"), Required]
         public string State { get; set; }
+
+        [Display(Name = "Zip Code:"), Required]
         public string ZipCode { get; set; }
+
+        [Display(Name = "County:"), Required]
         public string County { get; set; }
     }
 
-    public class MovieDBContext : DbContext
-    {
-        public DbSet<Address> Addresses { get; set; }
-    }
 }
