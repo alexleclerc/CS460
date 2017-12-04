48ff181911ddc0fcd83bfd1c9c167d7e956165e4
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-12-03 20:54:38 -0800

    Added JsonResult class to index, a jQuery file to handle the button 'onclick' function to Scripts.

--- homework8/Homework8/Controllers/HomeController.cs
+++ homework8/Homework8/Controllers/HomeController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework8.Models;
 
 namespace Homework8.Controllers
 {
     public class HomeController : Controller
     {
+        private ArtWorksContext db = new ArtWorksContext();
+
         public ActionResult Index()
         {
-            return View();
+
+            return View(db.Genres.ToList());
+
         }
+
+        public JsonResult Genre(int id)
+        {
+            var ArtCollection = db.Genres.Where(g => g.GenreID == id)
+                                         .Select(x => x.Classifications)
+                                         .FirstOrDefault()
+                                         .Select(x => new { x.Artwork.Title, x.Artwork.Artist.FirstName, x.Artwork.Artist.MiddleName, x.Artwork.Artist.LastName })
+                                         .OrderBy(x => x.Title)
+                                         .ToList();
+            //list object 
+            return Json(ArtCollection, JsonRequestBehavior.AllowGet);
+         }
     }
 }
---
+++ homework8/Homework8/Scripts/getWorks.js
@@ -0,0 +1,15 @@
+﻿function getWorks(id){
+    $.ajax(
+        {
+            type: "GET",
+            dataType: "json",
+            url: "Home/Genre/" + id,
+            success: (function (data) { displayArt(data) })
+        }
+    );
+
+    function displayArt(inData)
+    {
+
+    }
+ }
