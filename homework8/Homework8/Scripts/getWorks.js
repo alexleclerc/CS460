ae69c8b3559e14ec3d925d5779f53638d2f4d780
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-12-04 12:41:50 -0800

    Genre buttons functioning, jQuery building table properly.

--- homework8/Homework8/Scripts/getWorks.js
+++ homework8/Homework8/Scripts/getWorks.js
@@ -1,15 +1,40 @@
-﻿function getWorks(id){
+﻿function getWorks(id) {
     $.ajax(
         {
             type: "GET",
             dataType: "json",
-            url: "Home/Genre/" + id,
+            url: "/Home/Genre/" + id,
             success: (function (data) { displayArt(data) })
         }
     );
+}
 
     function displayArt(inData)
     {
+        $("#displayContainer").empty();
+        //create a string for the start of our table
+        var tableHTML = "<table class = \"table\"><thead><tr><th>Title</th><th>First Name</th><th>Middle Name</th><th>Last Name</th></tr></thead><tbody>";
+
+        //$("#displayContainer").append(tableStart);
+        //$("#displayContainer").append(tableHead);
+        $.each(inData, function (i, name) {
+          //  $("#displayContainer").append(
+                tableHTML += "<tr>"
+                + "<td>"
+                + name["Title"]
+                + "</td>"
+                + "<td>"
+                + name["FirstName"]
+                + "</td>"
+                + "<td>"
+                + name["MiddleName"]
+                + "</td>"
+                + "<td>"
+                + name["LastName"]
+                + "</td>"
+                + "</tr>";
+        });
+        tableHTML += "</tbody></table>";
+        $("#displayContainer").append(tableHTML);
 
     }
- }
