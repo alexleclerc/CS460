934034ef4e04ad1a33017a55fc992efd94a249c8
Alex LeClerc <aleclerc15@mail.wou.edu>
2017-12-03 19:10:40 -0800

    Got rid of Artworks CRUD, added date validation on Artist Model > BirthDate

--- homework8/Homework8/Models/Artist.cs
+++ homework8/Homework8/Models/Artist.cs
@@ -16,17 +16,20 @@ namespace Homework8.Models
 
         public int ArtistID { get; set; }
 
-        [StringLength(100)]
+        [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters.")]
         public string FirstName { get; set; }
 
-        [StringLength(100)]
+        [StringLength(50, ErrorMessage = "Middle name can't be longer than 50 characters.")]
         public string MiddleName { get; set; }
 
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters.")]
         public string LastName { get; set; }
 
         [Column(TypeName = "date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:MM/dd/yyyy}")]
+        [RestrictedDate]
         public DateTime? BirthDate { get; set; }
 
         [StringLength(100)]
@@ -34,5 +37,14 @@ namespace Homework8.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Artwork> Artworks { get; set; }
+
+        public class RestrictedDate : ValidationAttribute
+        {
+            public override bool IsValid(object date)
+            {
+                DateTime Date = (DateTime)date;
+                return Date < DateTime.Now;
+            }
+        }
     }
 }
